26a0ad9954b1b4ca3769b87d8c537982d79165c2
lbezerra <lbezerra@criptowow>
2022-07-27 18:29:30 +0000

    Ajuste para teste de unidade

---
+++ Spotify.Test/Application/BandaServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Moq;
+using Spotify.Application.Album.Dto;
+using Spotify.Application.Album.Service;
+using Spotify.Domain.Album;
+using Spotify.Domain.Album.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify.Test.Application
+{
+    public class BandaServiceTests
+    {
+        
+
+        [Fact]
+        public async Task DeveCriarBandaComSucesso()
+        {
+            BandaInputDto dtoInput = new BandaInputDto("XTPO", "https://xpto.com/foto.png", "Lorem ipsum da banda");
+            BandaOutputDto dtoOutput = new BandaOutputDto(Guid.NewGuid(), "XTPO", "https://xpto.com/foto.png", "Lorem ipsum da banda");   
+            
+
+            Mock<IBandaRepository> mockRepository = new Mock<IBandaRepository>();
+            Mock<IMapper> mockMapper = new Mock<IMapper>();
+
+            Banda banda = new Banda()
+            {
+                Descricao = "Lorem Ipsom",
+                Foto = "lorem ipsum",
+                Nome = "Xpto"
+            };
+
+            mockMapper.Setup(x => x.Map<Banda>(dtoInput)).Returns(banda);
+            mockMapper.Setup(x => x.Map<BandaOutputDto>(banda)).Returns(dtoOutput);
+
+            mockRepository.Setup(x => x.Save(It.IsAny<Banda>())).Returns(Task.FromResult(banda));
+
+            var service = new BandaService(mockRepository.Object, mockMapper.Object);
+            var result = await service.Criar(dtoInput);
+
+            Assert.NotNull(result);
+
+
+        }
+
+    }
+}
