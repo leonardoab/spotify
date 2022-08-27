9db0939f434c72f7593febb4518eb38c2dae6a3c
lbezerra <lbezerra@criptowow>
2022-08-27 16:28:08 +0000

    Colocando o banco remoto

--- Spotify.Repository/Migrations/20220827133525_criarbase.Designer.cs
+++ Spotify.Repository/Migrations/20220827133525_criarbase.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Spotify.Repository.Context;
 
@@ -11,9 +12,10 @@ using Spotify.Repository.Context;
 namespace Spotify.Repository.Migrations
 {
     [DbContext(typeof(SpotifyContext))]
-    partial class SpotifyContextModelSnapshot : ModelSnapshot
+    [Migration("20220827133525_criarbase")]
+    partial class criarbase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
